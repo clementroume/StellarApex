c18611284521022b9344fca6fa16f706c97e2596
Clément Roume <clement.roume@hotmail.com>
2026-02-23 14:02:09 +0100

    Otel logback documentation

--- antares-auth/src/main/java/apex/stellar/antares/config/InstallOpenTelemetryAppender.java
+++ antares-auth/src/main/java/apex/stellar/antares/config/InstallOpenTelemetryAppender.java
@@ -5,15 +5,27 @@ import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppen
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
+/**
+ * Spring component responsible for integrating OpenTelemetry with Logback.
+ *
+ * <p>This class ensures that the {@link OpenTelemetryAppender} is initialized with the provided
+ * {@link OpenTelemetry} instance once the Spring application context is ready.
+ */
 @Component
 class InstallOpenTelemetryAppender implements InitializingBean {
 
   private final OpenTelemetry openTelemetry;
 
+  /**
+   * Constructs a new instance with the required OpenTelemetry SDK.
+   *
+   * @param openTelemetry the OpenTelemetry instance to be used for log instrumentation
+   */
   InstallOpenTelemetryAppender(OpenTelemetry openTelemetry) {
     this.openTelemetry = openTelemetry;
   }
 
+  /** Installs the OpenTelemetry appender into the Logback configuration. */
   @Override
   public void afterPropertiesSet() {
     OpenTelemetryAppender.install(this.openTelemetry);
